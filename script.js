94bbae863bbba310743453e725e1e8143c5177ca
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-10-08 11:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	s = s.trimStart();
+	let i = 0;
+	while(i < s.length && s[i].charCodeAt() !== ' '.charCodeAt()){
+		i++;
+	}
+	return s.substr(0, i);
 }
 
 // Do not change the code below
